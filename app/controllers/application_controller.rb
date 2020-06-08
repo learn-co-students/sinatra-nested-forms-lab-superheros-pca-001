483f14c706db5f3c1e389a355ccf40566276091d
Aaron Wilson <adub65@gmail.com>
2020-06-08 14:29:55 -0400

    Sinatra nested forms with superheroes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  set :views, Proc.new { File.join(root, "../views/") }
 
+  get "/" do
+    erb :super_hero
+  end
 
+  post "/teams" do
+    @team = params[:team]
+    @members = params[:team][:members].map do |member|
+      Hero.new(member)
+    end
+
+    erb :team
+  end
 end
